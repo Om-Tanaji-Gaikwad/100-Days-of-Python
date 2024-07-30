5e14f382716c13c246e20c652a7e32271417dca0
Om-Tanaji-Gaikwad <omgaikwad9028@gmail.com>
2024-07-30 18:37:47 +0530

    Added day 01 project

--- day-01.py
+++ day-01.py
@@ -1,5 +1,5 @@
 # Band name Generator
 print("Welcome to band name Generator")
-a=input("Enter the name of your city : ")
-b=input("Enter the name of your favourite pet or animal : ")
-print("Your band name could be : ",a,b)
+a = input("Enter the name of your city : ")
+b = input("Enter the name of your favourite pet or animal : ")
+print("Your band name could be : ", a, b)
