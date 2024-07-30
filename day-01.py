aec6df1a10a1a9dc175e87295c28e5062455f6f6
Om-Tanaji-Gaikwad <omgaikwad9028@gmail.com>
2024-07-30 18:30:15 +0530

    Added day 01 project

---
+++ day-01.py
@@ -0,0 +1,5 @@
+# Band name Generator
+print("Welcome to band name Generator")
+a=input("Enter the name of your city : ")
+b=input("Enter the name of your favourite pet or animal : ")
+print("Your band name could be : ",a,b)
